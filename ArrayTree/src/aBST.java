b308db3efe34d1864ba89a99d09c8e32ae5d7b0a
zrdpsh <zerdposh@gmail.com>
2024-05-30 16:18:51 +0300

    complete exercise

--- ArrayTree/src/aBST.java
+++ ArrayTree/src/aBST.java
@@ -6,7 +6,7 @@ class aBST
 
     public aBST(int depth)
     {
-        int tree_size = 2^(depth+1) - 1;
+        int tree_size = 2^(depth+2) - 1;
         Tree = new Integer[ tree_size ];
         for(int i=0; i<tree_size; i++) Tree[i] = null;
     }

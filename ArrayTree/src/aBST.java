f295ef38e6ac2783816d3cdcbed4b2840d4b19f3
zrdpsh <zerdposh@gmail.com>
2024-05-30 03:01:30 +0300

    complete exercise

--- ArrayTree/src/aBST.java
+++ ArrayTree/src/aBST.java
@@ -2,27 +2,93 @@ import java.util.*;
 
 class aBST
 {
-    public Integer Tree []; // массив ключей
+    public Integer Tree [];
 
     public aBST(int depth)
     {
-        // правильно рассчитайте размер массива для дерева глубины depth:
-        int tree_size = 0;
+        int tree_size = 2^(depth+1) - 1;
         Tree = new Integer[ tree_size ];
         for(int i=0; i<tree_size; i++) Tree[i] = null;
     }
 
+
+
+
+
     public Integer FindKeyIndex(int key)
     {
-        // ищем в массиве индекс ключа
-        return null; // не найден
+        if (Tree[0] == null) return 0;
+
+        int currentIndex = 0;
+
+        for (;Tree[currentIndex] != key && getRightBranch(currentIndex) < Tree.length;)  {
+            if (key < Tree[currentIndex]) {
+                int leftIndex = getLeftBranch(currentIndex);
+
+                if (Tree[leftIndex] == null) return -leftIndex;
+
+                currentIndex = leftIndex;
+            }
+
+            if (key > Tree[currentIndex]) {
+                int rightIndex = getRightBranch(currentIndex);
+
+                if (Tree[rightIndex] == null) return -rightIndex;
+
+                currentIndex = rightIndex;
+            }
+        }
+
+        return Tree[currentIndex] == key ? currentIndex : null;
     }
+
+
+
+
+    private int getLeftBranch(int givenIndex) {return givenIndex*2+1;}
+
+    private int getRightBranch(int givenIndex) {return givenIndex*2+2;}
+
+
+
+
 
     public int AddKey(int key)
     {
-        // добавляем ключ в массив
+        if (Tree[0] == null) {
+            Tree[0] = key;
+            return 0;
+        }
+
+        int currentIndex = 0;
+
+        for (;getRightBranch(currentIndex) < Tree.length;)  {
+            if (key < Tree[currentIndex]) {
+                int leftIndex = getLeftBranch(currentIndex);
+
+                if (Tree[leftIndex] == null) {
+                    Tree[leftIndex] = key;
+                    return leftIndex;
+                }
+                currentIndex = leftIndex;
+            }
+
+            if (Tree[currentIndex] == key) return currentIndex;
+
+            if (key > Tree[currentIndex]) {
+                int rightIndex = getRightBranch(currentIndex);
+
+                if (Tree[rightIndex] == null) {
+                    Tree[rightIndex] = key;
+                    return rightIndex;
+                }
+
+                currentIndex = rightIndex;
+            }
+        }
         return -1;
-        // индекс добавленного/существующего ключа или -1 если не удалось
     }
+
+
 
 }
